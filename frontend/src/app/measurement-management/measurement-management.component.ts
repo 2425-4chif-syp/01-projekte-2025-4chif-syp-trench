bc993ee8b51f187f2bc711c0ae2f1f0734b1c82b
Ascyt <account@ascyt.com>
2024-12-01 21:10:39 +0100

    Added empty measurement-management component

---
+++ frontend/src/app/measurement-management/measurement-management.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-measurement-management',
+  standalone: true,
+  imports: [],
+  templateUrl: './measurement-management.component.html',
+  styleUrl: './measurement-management.component.scss'
+})
+export class MeasurementManagementComponent {
+
+}
