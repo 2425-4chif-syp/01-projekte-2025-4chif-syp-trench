ff28dbf44313c5f0395e30d55a9ec813229db687
aaronschreiegg <a.schreiegg@students.htl-leonding.ac.at>
2024-12-10 17:55:36 +0100

    feat: added combobox for Joch-amount selection and displaying right svg #trench-15 In Progress

--- frontend/src/app/coil-management/coil-management.component.ts
+++ frontend/src/app/coil-management/coil-management.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-coil-management',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './coil-management.component.html',
   styleUrl: './coil-management.component.scss'
 })
 export class CoilManagementComponent {
+  selectedNumber: number | null = null;
 
+  getSvgPath(): string {
+    return `assets/svg/${this.selectedNumber}RJ.svg`;
+  }
 }
