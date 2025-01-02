27133ea81659504bc53d3c40df636a21823fac09
Ascyt <account@ascyt.com>
2025-01-02 22:37:16 +0100

    feat: Implemented addNewCoil method and added coil selection handling #trench-23

--- frontend/src/app/coil-data/coils.service.ts
+++ frontend/src/app/coil-data/coils.service.ts
@@ -8,4 +8,22 @@ export class CoilsService {
   public coils: Coil[] = [];
 
   constructor() { }
+
+  public addNewCoil():Coil {
+    const newId:number = this.coils.map(c => c.id).reduce((a, b) => Math.max(a, b), 0) + 1;
+    
+    const newCoil:Coil = {
+      id: newId,
+      name: 'Spule #' + newId,
+      diameter: 1,
+      arcLength: 1,
+      endArea: 1,
+      tolerance: 0,
+      yokesCount: 2
+    };
+
+    this.coils.push(newCoil);
+
+    return newCoil;
+  }
 }
--- frontend/src/app/coil-management/coil-management.component.ts
+++ frontend/src/app/coil-management/coil-management.component.ts
@@ -14,7 +14,7 @@ import { CoilsService } from '../coil-data/coils.service';
 export class CoilManagementComponent {
   constructor(public coilsService:CoilsService) {}  
 
-  selectedCoil:Coil|undefined = undefined;
+  selectedCoilId: number | null = null;
 
   yokesCount: number = 2; // Standardwert für die Jochanzahl
   name: string = ''; // Spulenname
@@ -27,7 +27,56 @@ export class CoilManagementComponent {
     return `assets/svg/${this.yokesCount}RJ.svg`;
   }
 
-  addNewCoil() { 
+  addNewCoil() {
+    const newCoil: Coil = this.coilsService.addNewCoil();
+    this.selectedCoilId = newCoil.id;
+    this.onSelectedCoilChange(newCoil.id);
+  }
+
+  saveChanges() {
+    if (this.selectedCoilId === null) {
+      return;
+    }
+
+    if (typeof this.selectedCoilId !== 'number') {
+      // This can happen if Angular sets selectedCoilId to a string for some reason
+      throw new Error('selectedCoilId is not of type number'); 
+    }
+
+    const coil:Coil|undefined = this.coilsService.coils.find(c => c.id === this.selectedCoilId!);
+
+    if (coil === undefined) {
+      throw new Error(`Coil with ID ${this.selectedCoilId} not found`);
+    }
+
+    coil.name = this.name;
+    coil.yokesCount = this.yokesCount;
+    // TODO: Check if the following properties are not null, if they are tell the user to fill them in
+    coil.diameter = this.diameter??0;
+    coil.arcLength = this.arcLength??0;
+    coil.endArea = this.endArea??0;
+    coil.tolerance = this.tolerance;
+
+    this.onSelectedCoilChange(this.selectedCoilId);
+  }
+
+  onSelectedCoilChange(coilId: number) {
+    // Not sure why I have to cast the coilId to a number here, but it seems to be necessary. 
+    // Angular seems to pass the coilId as a string, despite what the type definition says.
+    const coilIdNumber:number = Number(coilId);
+    this.selectedCoilId = coilIdNumber;
     
+    const coil = this.coilsService.coils.find(c => c.id === coilIdNumber);
+    
+    if (coil === undefined) {
+      return;
+    }
+
+    this.name = coil.name;
+    this.yokesCount = coil.yokesCount;
+    this.diameter = coil.diameter;
+    this.arcLength = coil.arcLength;
+    this.endArea = coil.endArea;
+    this.tolerance = coil.tolerance;
   }
 }
