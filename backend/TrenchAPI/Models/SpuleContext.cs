478ecc059b0724dad55e3e9adce6993c5060345f
Emil-Silber <emil.silber06@gmail.com>
2025-01-03 22:22:16 +0100

    feat_add-context

---
+++ backend/TrenchAPI/Models/SpuleContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TrenchAPI.Models
+{
+    public class SpuleContext : DbContext
+    {
+        public SpuleContext(DbContextOptions<SpuleContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Spule> Spule { get; set; }
+    }
+}
