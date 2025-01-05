d67656561cd91d39ed4a20fe906e80a9c5d7f9e1
Ascyt <account@ascyt.com>
2025-01-05 20:00:42 +0100

    feat: Implement CoilListComponent with clickable table #trench-28

--- frontend/src/app/coil-management/coil-list/coil-list.component.ts
+++ frontend/src/app/coil-management/coil-list/coil-list.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CoilsService } from '../../data/coil-data/coils.service';
+import { Coil } from '../../data/coil-data/coil';
 
 @Component({
   selector: 'app-coil-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './coil-list.component.html',
   styleUrl: './coil-list.component.scss'
 })
 export class CoilListComponent {
+  constructor(public coilsService:CoilsService) {}
 
+  addNewCoil() {
+    const newCoil: Coil = this.coilsService.addNewCoil();
+
+    //this.coilsService.selectCoil(newCoil.id);
+  }
+
+  openCoil(coilId:number) {
+    this.coilsService.selectCoil(coilId);
+  }
 }
