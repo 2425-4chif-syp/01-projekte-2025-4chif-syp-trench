cf0ff5563a33425948f0cd9dfbe24d1c3c0f7382
Ascyt <account@ascyt.com>
2025-01-07 17:47:22 +0100

    feat: enhance BackendService with HTTP request handling and coil data formatting #trench-28

--- frontend/src/app/backend.service.ts
+++ frontend/src/app/backend.service.ts
@@ -1,24 +1,47 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { Coil } from './data/coil-data/coil';
 
 @Injectable({
   providedIn: 'root'
 })
-export class BackendService {    
+export class BackendService {
   private apiUrl = 'http://localhost:5127/api/';
 
-  constructor(private httpClient:HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
-  public getAllCoils():Promise<any>|void  {
+  // Endpoint is the path to the API endpoint (not starting with /), e.g. 'Spule'
+  private httpGetRequest(endpoint:string): Promise<any> | void {
     return new Promise<any>((resolve, reject) => {
-      this.httpClient.get(this.apiUrl + 'Spule').subscribe({
+      this.httpClient.get(this.apiUrl + endpoint).subscribe({
         next: (response) => {
           resolve(response);
         },
         error: (error) => {
-            reject(error);
+          reject(error);
         }
+      });
     });
-    });
+  }
+
+  private formatCoil(coil: any): Coil { 
+    return {
+      id: coil.id,
+      ur: coil.ur,
+      einheit: coil.einheit,
+      auftragsnummer: coil.auftragsnummer,
+      auftragsPosNr: coil.auftragsPosNr,
+      omega: coil.omega,
+    };
+  }
+
+  public async getAllCoils(): Promise<Coil[]> {
+    const response:any = await this.httpGetRequest('Spule');
+    return response.map((coil: any) => (this.formatCoil(coil)));
+  }
+
+  public async getCoil(id: number): Promise<Coil> {
+    const response:any = await this.httpGetRequest('Spule/' + id);
+    return this.formatCoil(response);
   }
 }
