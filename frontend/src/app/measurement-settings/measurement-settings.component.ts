af36203daff31bd36955c31a2ab002a79e12205f
aaronschreiegg <a.schreiegg@students.htl-leonding.ac.at>
2025-01-30 21:13:35 +0100

    feat: Added measurement-settings input fields #trench-55 In Progress

--- frontend/src/app/measurement-settings/measurement-settings.component.ts
+++ frontend/src/app/measurement-settings/measurement-settings.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-measurement-settings',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './measurement-settings.component.html',
   styleUrl: './measurement-settings.component.scss'
 })
 export class MeasurementSettingsComponent {
+  schenkelAnzahl = signal<number[]>([1, 2, 3, 4, 5]) 
+  saveMessage: string | null = null
 
+  measurementSettings = {
+    bemessungsSpannung: 0,
+    bemessungsFrequenz: 0,
+    sondenProSchenkel: 1,
+    messStaerke: 0
+  };
+
+  saveChanges() {
+    console.log('Einstellungen gespeichert:', this.measurementSettings);
+    this.saveMessage = 'Änderungen gespeichert!';
+    setTimeout(() => {
+      this.saveMessage = null;
+    }, 3000);
+  }  
 }
