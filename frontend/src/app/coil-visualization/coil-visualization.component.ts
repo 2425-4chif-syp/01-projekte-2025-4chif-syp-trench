21b92bef086a68a8ba62736c23a3b33758179ae8
Ascyt <account@ascyt.com>
2025-02-02 18:10:58 +0100

    feat: Add coil visualization component and integrate it into coil type management #trench-60

---
+++ frontend/src/app/coil-visualization/coil-visualization.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coil-visualization',
+  standalone: true,
+  imports: [],
+  templateUrl: './coil-visualization.component.html',
+  styleUrl: './coil-visualization.component.scss'
+})
+export class CoilVisualizationComponent {
+
+}
