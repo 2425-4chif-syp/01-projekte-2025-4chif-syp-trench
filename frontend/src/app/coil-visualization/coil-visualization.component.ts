754d8c42cb1cda54506b4afd611cf6957c1ad082
Ascyt <account@ascyt.com>
2025-02-02 19:30:58 +0100

    feat: Enhance coil visualization with dynamic coil visualization rendering and input binding #trench-60

--- frontend/src/app/coil-visualization/coil-visualization.component.ts
+++ frontend/src/app/coil-visualization/coil-visualization.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-coil-visualization',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './coil-visualization.component.html',
   styleUrl: './coil-visualization.component.scss'
 })
 export class CoilVisualizationComponent {
+  @Input() n:number = 2;
 
+  public get nTransforms():any[] {
+    let polylines = [];
+    for (let i = 0; i < this.n; i++) {
+      polylines.push(this.getNTransform(i));
+    }
+    console.log(polylines);
+    return polylines;
+  }
+
+  public getNTransform(n:number):any {
+    return {
+      n: n,
+      rotate: 360 / this.n * n
+    }
+  }
 }
