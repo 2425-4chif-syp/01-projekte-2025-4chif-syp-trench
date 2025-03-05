652b3e22f001362493d8d3a0cf711b3941e0bbea
aaronschreiegg <a.schreiegg@students.htl-leonding.ac.at>
2025-03-05 16:04:40 +0100

    chore: swapped code to new angular syntax #trench-83

--- frontend/src/app/measurement-settings/measurement-probe-management/measurement-probe-management.component.ts
+++ frontend/src/app/measurement-settings/measurement-probe-management/measurement-probe-management.component.ts
@@ -1,17 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MeasurementProbe } from '../../data/measurement-probes/measurement-probes';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-measurement-probe-management',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './measurement-probe-management.component.html',
   styleUrl: './measurement-probe-management.component.scss'
 })
 export class MeasurementProbeManagementComponent {
-
   private yokeAmount = 4;
+
 
   measurementProbes: MeasurementProbe[] = [
     { id: 1, width: 50, yoke: 1, position: 1 },
@@ -28,7 +29,7 @@ export class MeasurementProbeManagementComponent {
     { id: 12, width: 50, yoke: 4, position: 3 },    
   ];
 
-  groupedProbes: MeasurementProbe[][] = [];
+  groupedProbes = signal<MeasurementProbe[][]>([]);
 
   ngOnInit(): void {
     this.groupSensors();
@@ -40,14 +41,14 @@ export class MeasurementProbeManagementComponent {
       // Gesamtzahl der Jochs (alle Sensoren haben denselben Wert)
       const yokeCount = this.measurementProbes[0];
       // Initialisiere das Array für die Gruppen (z. B. 3 leere Arrays)
-      this.groupedProbes = Array.from({ length: this.yokeAmount }, () => []);
+      this.groupedProbes.set(Array.from({ length: this.yokeAmount }, () => []));
 
       // Gehe alle Sensoren durch und sortiere sie in die jeweilige Gruppe ein
       for (const probe of this.measurementProbes) {
         // Da 'position' 1-indexiert ist, ziehen wir 1 ab
         const index = probe.yoke - 1;
         // Sensor in die entsprechende Gruppe einfügen
-        this.groupedProbes[index].push(probe);
+        this.groupedProbes()[index].push(probe);
       } 
     }
   }
