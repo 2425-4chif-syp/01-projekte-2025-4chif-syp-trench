c89fe806ab7bb41cafbc22b743ef80c485c71218
SimonPesut <s.pesut@students.htl-leonding.ac.at>
2025-03-06 10:00:23 +0100

    feat: Added if to links to check if logged in #trench-82 In Progress

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { Router, NavigationEnd, RouterModule, RouterOutlet } from '@angular/router';
+import { AuthService } from './services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, CommonModule],
+  providers: [AuthService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'asc-visualization';
+  isLoginPage = false; 
+
+  constructor(public authService: AuthService, private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.isLoginPage = event.url.split('?')[0] === '/login';
+      }
+    });
+  }
+
 }
