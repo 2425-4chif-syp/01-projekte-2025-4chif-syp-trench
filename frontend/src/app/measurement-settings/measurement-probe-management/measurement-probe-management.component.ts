9c7f0eed7d719c38629ed67d6e43016fa9fe893f
aaronschreiegg <a.schreiegg@students.htl-leonding.ac.at>
2025-03-11 20:48:51 +0100

    fix: fixed probe getting created even when not saving #trench-83

--- frontend/src/app/measurement-settings/measurement-probe-management/measurement-probe-management.component.ts
+++ frontend/src/app/measurement-settings/measurement-probe-management/measurement-probe-management.component.ts
@@ -9,7 +9,7 @@ import { AppComponent } from '../../app.component';
 @Component({
   selector: 'app-measurement-probe-management',
   standalone: true,
-  imports: [CommonModule, FormsModule], 
+  imports: [CommonModule, FormsModule],
   templateUrl: './measurement-probe-management.component.html',
   styleUrl: './measurement-probe-management.component.scss',
 })
@@ -32,7 +32,7 @@ export class MeasurementProbeManagementComponent {
     { id: 9, width: 50, yoke: 3, position: 3 },
     { id: 10, width: 50, yoke: 4, position: 1 },
     { id: 11, width: 50, yoke: 4, position: 2 },
-    { id: 12, width: 50, yoke: 4, position: 3 },    
+    { id: 12, width: 50, yoke: 4, position: 3 },
   ];
 
   groupedProbes = signal<MeasurementProbe[][]>([]);
@@ -49,12 +49,12 @@ export class MeasurementProbeManagementComponent {
       for (const probe of this.measurementProbes) {
         const index = probe.yoke - 1;
         this.groupedProbes()[index].push(probe);
-      } 
+      }
     }
   }
 
   openModal(probe: any): void {
-    this.selectedProbe = {...probe};
+    this.selectedProbe = { ...probe };
   }
 
   closeModal(): void {
@@ -65,13 +65,28 @@ export class MeasurementProbeManagementComponent {
   saveChanges(): void {
     const index = this.measurementProbes.findIndex((probe) => probe.id === this.selectedProbe!.id);
 
-    if(!this.isValidProbe(this.selectedProbe!)){
+    if (!this.isValidProbe(this.selectedProbe!)) {
       this.errorMessage = "Bitte gültige Werte eingeben! Keine leeren Felder, keine 0 oder negativen Zahlen.";
       return;
     }
 
-    if (index !== -1 && this.selectedProbe) {
-      this.measurementProbes[index] = { ...this.selectedProbe };
+    if (this.isNewProbe) {
+      this.measurementProbes.push({
+        id: this.selectedProbe!.id!,
+        width: this.selectedProbe!.width!,
+        yoke: this.selectedProbe!.yoke!,
+        position: this.selectedProbe!.position!
+      } as MeasurementProbe);
+    } else {
+      const index = this.measurementProbes.findIndex(probe => probe.id === this.selectedProbe!.id);
+      if (index !== -1) {
+        this.measurementProbes[index] = {
+          id: this.selectedProbe!.id!,
+          width: this.selectedProbe!.width!,
+          yoke: this.selectedProbe!.yoke!,
+          position: this.selectedProbe!.position!
+        } as MeasurementProbe;
+      }
     }
 
     this.closeModal();
@@ -89,11 +104,11 @@ export class MeasurementProbeManagementComponent {
     );
   }
 
-  addMeasurementProbe(yoke: number): void{
+  addMeasurementProbe(yoke: number): void {
     const existingProbes = this.measurementProbes.filter(probe => probe.yoke === (yoke + 1));
-    const maxPosition = existingProbes.length > 0 
-        ? Math.max(...existingProbes.map(probe => probe.position)) 
-        : 0;
+    const maxPosition = existingProbes.length > 0
+      ? Math.max(...existingProbes.map(probe => probe.position))
+      : 0;
 
     const newProbe = {
       id: this.measurementProbes.length + 1,
@@ -104,10 +119,6 @@ export class MeasurementProbeManagementComponent {
     this.isNewProbe = true;
 
     this.openModal(newProbe);
-
-    if (this.isValidProbe(newProbe)) {
-      this.measurementProbes.push(newProbe);
-      this.groupSensors();
-    }
+    this.selectedProbe = newProbe;
   }
 }
