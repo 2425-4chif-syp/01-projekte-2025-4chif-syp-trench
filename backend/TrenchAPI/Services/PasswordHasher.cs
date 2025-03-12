d4eb787ff0fc561b4c001bcffcd6ae225c537902
SimonPesut <s.pesut@students.htl-leonding.ac.at>
2025-03-12 11:24:19 +0100

    fix: Added Semicolon

--- backend/TrenchAPI/Services/PasswordHasher.cs
+++ backend/TrenchAPI/Services/PasswordHasher.cs
@@ -9,6 +9,6 @@ public class PasswordHasher
 
     public static bool VerifyPassword(string password, string hashedPassword)
     {
-        return hashedPassword is not null && BCrypt.Net.BCrypt.Verify(password, hashedPassword)
+        return hashedPassword is not null && BCrypt.Net.BCrypt.Verify(password, hashedPassword);
     }
 }
