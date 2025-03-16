30b4799e2ca5d3d6856d599031a6aa83e6282a23
Emil-Silber <emil.silber06@gmail.com>
2025-03-16 12:52:35 +0100

    feat: add-gesamtmessung #trench-85

--- backend/TrenchAPI/Context/WebDbContext.cs
+++ backend/TrenchAPI/Context/WebDbContext.cs
@@ -12,6 +12,7 @@ namespace TrenchAPI.Context
         public DbSet<Spule> Spule { get; set; }
         public DbSet<SpuleTyp> SpuleTyp { get; set; }
         public DbSet<Messeinstellung> Messeinstellung { get; set; }
+        public DbSet<Gesamtmessung> Gesamtmessung { get; set; }
 
         public void onModelCreating(ModelBuilder modelBuilder)
         {
---
+++ backend/TrenchAPI/Models/Gesamtmessung.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TrenchAPI.Models
+{
+    public class Gesamtmessung
+    {
+        [Key]
+        [Column(TypeName = "int")]
+        public int GesamtmessungID { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(SpuleID))]
+        [Column(TypeName = "int")]
+        public Spule? Spule { get; set; }
+
+        public int SpuleID { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(8,3)")]
+        public decimal GesamtVektor { get; set; }
+
+        [Required]
+        [Column(TypeName = "timestamp")]
+        public DateTime Zeit { get; set; }
+    }
+}
