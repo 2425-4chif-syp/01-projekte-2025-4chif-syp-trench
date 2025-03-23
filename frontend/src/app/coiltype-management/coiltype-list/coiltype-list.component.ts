078e32c82bf707dcef96495188d552b96af7f8dd
Ascyt <account@ascyt.com>
2025-03-23 18:12:23 +0100

    feat: Implemented generic list in coiltype list #trench-92

--- frontend/src/app/coiltype-management/coiltype-list/coiltype-list.component.ts
+++ frontend/src/app/coiltype-management/coiltype-list/coiltype-list.component.ts
@@ -5,60 +5,46 @@ import { Coiltype } from '../../data/coiltype-data/coiltype';
 import { CoilsService } from '../../data/coil-data/coils.service';
 import { Router } from '@angular/router';
 import { CoilVisualizationComponent } from '../../coil-visualization/coil-visualization.component';
+import { GenericListComponent } from '../../generic-list/generic-list.component';
+import { LIST_SERVICE_TOKEN } from '../../data/list-service';
 
 @Component({
   selector: 'app-coiltype-list',
   standalone: true,
-  imports: [CommonModule, CoilVisualizationComponent],
+  imports: [CommonModule, CoilVisualizationComponent, GenericListComponent],
+  providers: [
+    {
+      provide: LIST_SERVICE_TOKEN,
+      useExisting: CoiltypesService
+    }
+  ],
   templateUrl: './coiltype-list.component.html',
   styleUrl: './coiltype-list.component.scss'
 })
 export class CoiltypeListComponent {
-  public sortedCoiltypes: Coiltype[] = [];
-
-  private sortDirection: { [key: string]: boolean } = {};
-
   public hoveredCoiltype: Coiltype | null = null;
-
   public mousePosition: { x: number, y: number }|null = null;
 
+  public readonly keysAsColumns: { [key: string]: string } = {
+    'id': 'ID',
+    'tK_Name': 'Name',
+    'schenkel': 'Schenkelanzahl',
+    'bb': 'Bandbreite',
+    'sh': 'Schichthöhe',
+    'dm': 'Durchmesser'
+  }
+
   constructor(public coiltypesService:CoiltypesService, public coilsService:CoilsService, private router:Router) {
-    this.initialize();
+
   }
 
   public get isCoilSelector(): boolean { 
     return this.coiltypesService.isCoilSelector;
   }
 
-  async initialize() {
-    await this.coiltypesService.reloadElements();
-    this.sortedCoiltypes = [...this.coiltypesService.elements]
-  }
+  public openCoiltype(coiltype:Coiltype) {
+    const coiltypeId = coiltype.id!;
 
-  onElementHoverStart(coiltype:Coiltype) {
-    this.hoveredCoiltype = coiltype;
-  }
-  onElementHoverEnd(coiltype:Coiltype) {
-    if (this.hoveredCoiltype === coiltype) {
-      this.hoveredCoiltype = null;
-    }
-  }
-
-  async addNewCoiltype() {
-    const newCoiltype:Coiltype = {
-      id: 0,
-      tK_Name: '',
-      schenkel: 0,
-      bb: 0,
-      sh: 0,
-      dm: 0
-    };
-
-    this.coiltypesService.selectedElementCopy = newCoiltype;
-    this.coiltypesService.selectedElementIsNew = true; 
-  }
-
-  openCoiltype(coiltypeId:number) {
     if (this.coiltypesService.isCoilSelector) {
       this.coilsService.selectedElementCopy!.coiltypeId = coiltypeId;
       this.coilsService.selectedElementCopy!.coiltype = this.coiltypesService.getCopyElement(coiltypeId);
@@ -70,22 +56,8 @@ export class CoiltypeListComponent {
     this.coiltypesService.selectElement(coiltypeId);
   }
 
-  sortTable(column: keyof Coiltype) {
-    const direction = this.sortDirection[column] = !this.sortDirection[column];
-
-    this.sortedCoiltypes.sort((a, b) => {
-      if (a[column]! < b[column]!) {
-        return direction ? -1 : 1;
-      }
-      if (a[column]! > b[column]!) {
-        return direction ? 1 : -1;
-      }
-      return 0;
-    });
-  }
-
-  @HostListener('document:mousemove', ['$event'])
-  onMouseMove(event: MouseEvent) {
-    this.mousePosition = { x: event.pageX, y: event.pageY };
+  public updateHoveredCoiltype(event:{element:Coiltype|null, mousePosition:{x:number, y:number}|null}) {
+    this.hoveredCoiltype = event.element;
+    this.mousePosition = event.mousePosition;
   }
 }
