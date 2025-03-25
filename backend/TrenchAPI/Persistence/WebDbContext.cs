72f77b67070bb8421c1cf868e66ec27802054b89
Emil-Silber <emil.silber06@gmail.com>
2025-03-25 15:46:17 +0100

    ref: delete-outdated-entities-and-controllers #trench-96

--- backend/TrenchAPI/Persistence/WebDbContext.cs
+++ backend/TrenchAPI/Persistence/WebDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using TrenchAPI.Core.Entities;
 
-namespace TrenchAPI.Context
+namespace TrenchAPI.Persistence
 {
     public class WebDbContext : DbContext
     {
@@ -12,10 +12,7 @@ namespace TrenchAPI.Context
         public DbSet<Spule> Spule { get; set; }
         public DbSet<SpuleTyp> SpuleTyp { get; set; }
         public DbSet<Messeinstellung> Messeinstellung { get; set; }
-        public DbSet<Sensormessung> Sensormessung { get; set; }
-        public DbSet<Gesamtmessung> Gesamtmessung { get; set; }
-        public DbSet<Sensor> Sensor { get; set; }
-        public DbSet<SensorTyp> SensorTyp { get; set; }
+        public DbSet<MesssondenTyp> MesssondenTyp { get; set; }
 
         public void onModelCreating(ModelBuilder modelBuilder)
         {
