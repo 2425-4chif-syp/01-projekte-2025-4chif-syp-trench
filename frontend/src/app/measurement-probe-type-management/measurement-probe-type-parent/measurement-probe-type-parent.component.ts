aecc9e4adb0b57767a8c5542cf6150d97f33e875
Ascyt <account@ascyt.com>
2025-03-30 21:12:46 +0200

    fix/refactor: Fixed measurement probe type management not opening, removed old code #trench-94

--- frontend/src/app/measurement-probe-type-management/measurement-probe-type-parent/measurement-probe-type-parent.component.ts
+++ frontend/src/app/measurement-probe-type-management/measurement-probe-type-parent/measurement-probe-type-parent.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MeasurementProbeTypeListComponent } from '../measurement-probe-type-list/measurement-probe-type-list.component';
 import { MeasurementProbeManagementComponent } from "../../measurement-settings/measurement-probe-management/measurement-probe-management.component";
 import { MeasurementProbeTypeManagementComponent } from '../measurement-probe-type-management/measurement-probe-type-management.component';
+import { MeasurementProbeTypesService } from '../../data/measurement-probe-type-data/measurement-probe-types.service';
 
 @Component({
   selector: 'app-measurement-probe-type-parent',
@@ -11,5 +12,5 @@ import { MeasurementProbeTypeManagementComponent } from '../measurement-probe-ty
   styleUrl: './measurement-probe-type-parent.component.scss'
 })
 export class MeasurementProbeTypeParentComponent {
-
+  constructor(public measurementProbeTypesService:MeasurementProbeTypesService) { }
 }
