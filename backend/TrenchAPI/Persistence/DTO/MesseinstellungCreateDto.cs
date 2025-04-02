97f4d1f35a4442c7f5d8d4835617002d27af87cb
Emil-Silber <emil.silber06@gmail.com>
2025-04-02 08:14:46 +0200

    feat: add-messeinstellung-dto #trench-106

---
+++ backend/TrenchAPI/Persistence/DTO/MesseinstellungCreateDto.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using TrenchAPI.Core.Entities;
+
+namespace TrenchAPI.Persistence.DTO
+{
+    public class MesseinstellungCreateDto : EntityObject
+    {
+        public int SpuleID { get; set; }
+        public int MesssondenTypID { get; set; }
+        public int Sonden_pro_schenkel { get; set; }
+        public decimal Bemessungsspannung { get; set; }
+        public decimal Bemessungsfrequenz { get; set; }
+        public string Notiz { get; set; } = "";
+    }
+}
