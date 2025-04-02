63d32f4157efdc2f23b4af27c2a4bfd414375a3a
aaronschreiegg <a.schreiegg@students.htl-leonding.ac.at>
2025-04-02 11:20:59 +0200

    feat: add id to measurement settings #trench-105

--- frontend/src/app/configuration/measurement-settings/interfaces/measurement-settings.ts
+++ frontend/src/app/configuration/measurement-settings/interfaces/measurement-settings.ts
@@ -2,6 +2,7 @@ import { Coil } from "../../coil/interfaces/coil";
 import { MeasurementProbeType } from "../../measurement-probe-type/interfaces/measurement-probe-type";
 
 export interface MeasurementSetting{
+    id: number | null,
     coil: Coil | null,
     coilId: number | null,
     measurementProbeType: MeasurementProbeType | null,
