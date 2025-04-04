5ea1ab7f6bc08538a59e0657d9419094393cccbf
aaronschreiegg <a.schreiegg@students.htl-leonding.ac.at>
2025-04-04 18:37:00 +0200

    feat: implemented model and backend services #trench-102

--- frontend/src/app/configuration/measurement-history/interfaces/measurement.model.ts
+++ frontend/src/app/configuration/measurement-history/interfaces/measurement.model.ts
@@ -1,4 +1,10 @@
+import { MeasurementSetting } from "../../measurement-settings/interfaces/measurement-settings";
+
 export interface Measurement {
-    id: number,
-    name: string
+    id: number | null,
+    measurementSettings: MeasurementSetting | null,
+    measurementSettingsId: number | null,
+    startTime: Date | null,
+    endTime: Date | null,
+    notiz: string | null,
 }
