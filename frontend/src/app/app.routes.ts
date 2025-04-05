40027bec68de1f795de6245abfd3eada4ab5fc1b
Ascyt <account@ascyt.com>
2025-04-05 15:24:22 +0200

    refactor: Removed old measurement-management and tolerance-settings components #trench-111

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,9 +4,7 @@ import { AuthGuard } from './auth/auth.guard';
 import { LoginComponent } from './auth/login/login.component';
 import { CoilParentComponent } from './configuration/coil/components/coil-parent.component';
 import { CoiltypeParentComponent } from './configuration/coiltype/components/coiltype-parent.component';
-import { MeasurementManagementComponent } from './configuration/measurement/components/measurement-management.component';
 import { MeasurementSettingsComponent } from './configuration/measurement-settings/components/management/measurement-settings.component';
-import { ToleranceSettingsComponent } from './configuration/tolerance-settings/components/tolerance-settings.component';
 import { DisplacementVisualizationComponent } from './visualization/displacement/components/displacement-visualization.component';
 import { MeasurementProbeTypeParentComponent } from './configuration/measurement-probe-type/components/measurement-probe-type-parent.component';
 import { StartMeasurementComponent } from './configuration/start-measurement/start-measurement.component';
@@ -18,11 +16,9 @@ export const routes: Routes = [
     {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
     {path: 'coil-management', component: CoilParentComponent, canActivate: [AuthGuard]},
     {path: 'coiltype-management', component: CoiltypeParentComponent, canActivate: [AuthGuard]},
-    {path: 'measurement-result', component: MeasurementManagementComponent, canActivate: [AuthGuard]},
     {path: 'measurement-settings', component: MeasurementSettingsComponent, canActivate: [AuthGuard]},
-    {path: 'tolerance-settings', component: ToleranceSettingsComponent, canActivate: [AuthGuard]},
     {path: 'displacement-visualization', component: DisplacementVisualizationComponent, canActivate: [AuthGuard]},
     {path: 'measurement-probe-type-management', component: MeasurementProbeTypeParentComponent, canActivate: [AuthGuard]},
-    {path: 'start-measurement', component: StartMeasurementComponent, canActivate: [AuthGuard]},
+    {path: 'measurement-management', component: StartMeasurementComponent, canActivate: [AuthGuard]},
     {path: 'measurement-settings-list', component: MeasurementSettingsParentComponent, canActivate: [AuthGuard]}
 ];
