51cfbaad77c99576ab748f6db091e46ec440181f
Emil-Silber <emil.silber06@gmail.com>
2025-05-06 15:05:19 +0200

    chore: add-sondenposition #trench-130

--- backend/TrenchAPI/Persistence/WebDbContext.cs
+++ backend/TrenchAPI/Persistence/WebDbContext.cs
@@ -15,8 +15,11 @@ namespace TrenchAPI.Persistence
         public DbSet<MesssondenTyp> MesssondenTyp { get; set; }
         public DbSet<Messung> Messung { get; set; }
         public DbSet<Messwert> Messwert { get; set; }
+
         public DbSet<Sonde> Sonde { get; set; }
+        public DbSet<SondenPosition> SondenPosition { get; set; }
         public DbSet<SondenTyp> SondenTyp { get; set; }
+
         public DbSet<Spule> Spule { get; set; }
         public DbSet<SpuleTyp> SpuleTyp { get; set; }
 
