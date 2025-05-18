28d6b372571c74016efd252c888ee22de3f872d2
Emil-Silber <emil.silber06@gmail.com>
2025-05-18 17:31:36 +0200

    chore: fix-messwert-controller #trench-130

--- backend/TrenchAPI/Core/Entities/Messwert.cs
+++ backend/TrenchAPI/Core/Entities/Messwert.cs
@@ -8,19 +8,14 @@ namespace TrenchAPI.Core.Entities
     {
         [Required]
         public int MessungID { get; set; }
-
         [ForeignKey(nameof(MessungID))]
         public virtual Messung Messung { get; set; }
-
         [Required]
         public int SondenPositionID { get; set; }
-
         [ForeignKey(nameof(SondenPositionID))]
         public virtual SondenPosition SondenPosition { get; set; }
-
         [Column(TypeName = "decimal")]
         public decimal Wert { get; set; }
-
         [Column(TypeName = "timestamp with time zone")]
         public DateTime Zeitpunkt { get; set; }
     }
