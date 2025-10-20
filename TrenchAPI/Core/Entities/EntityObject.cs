1a277676c69f1ce5f700ad393575d1433d1a4ddb
Emil-Silber <emil.silber06@gmail.com>
2025-10-20 09:27:48 +0200

    refactor: core

---
+++ TrenchAPI/Core/Entities/EntityObject.cs
@@ -0,0 +1,12 @@
+﻿using TrenchAPI.Core.Contracts;
+using System.ComponentModel.DataAnnotations;
+
+namespace TrenchAPI.Core.Entities
+{
+
+    public class EntityObject : IEntityObject
+    {
+        [Key]
+        public int ID { get; set; }
+    }
+}
---
+++ TrenchAPI/Core/Entities/SpuleTyp.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TrenchAPI.Core.Entities
+{
+    public class SpuleTyp : EntityObject
+    {
+        [Column(TypeName = "text")]
+        public string Name { get; set; } = "";
+
+        [Column(TypeName = "int")]
+        public int Schenkelzahl { get; set; }
+
+        [Column(TypeName = "decimal")]
+        public decimal Bandbreite { get; set; }
+
+        [Column(TypeName = "decimal")]
+        public decimal Schichthoehe { get; set; }
+
+        [Column(TypeName = "decimal")]
+        public decimal Durchmesser { get; set; }
+
+        [Column(TypeName = "int")]
+        public int Toleranzbereich { get; set; }
+
+        [Column(TypeName = "text")]
+        public string Notiz { get; set; } = "";
+    }
+}
